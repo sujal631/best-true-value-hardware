ad4586a5e4166d231f70dba4ee780f7492a50208
sujal631 <99301344+sujal631@users.noreply.github.com>
2023-01-30 17:19:04 -0600

    create react app for btvh

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Best True Value Hardware</a>
+      </header>
+      <main>LIST PRODUCTS</main>
+    </div>
+  );
+}
+
+export default App;
