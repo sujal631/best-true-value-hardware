364103d6c3aa99955d05e759ea08f32ab8c8c84d
sujal631 <99301344+sujal631@users.noreply.github.com>
2023-01-30 23:45:50 -0600

    reducer-logger removed

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,7 +1,6 @@
 import { useEffect, useReducer } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-import logger from 'use-reducer-logger';
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -17,7 +16,7 @@ const reducer = (state, action) => {
 };
 
 function HomePage() {
-  const [{ loading, error, product }, dispatch] = useReducer(logger(reducer), {
+  const [{ loading, error, product }, dispatch] = useReducer(reducer, {
     product: [],
     loading: true,
     error: '',
