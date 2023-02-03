30f37497044dc24559848a6e08d1de4eac4b55f4
sujal631 <99301344+sujal631@users.noreply.github.com>
2023-02-02 18:35:42 -0600

    abcd

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -46,7 +46,7 @@ function HomePage() {
         ) : (
           <Row>
             {products.map((product) => (
-              <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
+              <Col key={product.slug} sm={12} md={6} lg={3} className="mb-3">
                 <Product product={product} />
               </Col>
             ))}
