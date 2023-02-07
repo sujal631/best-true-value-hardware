68e0b2946aa2d46fd6e579dced5549e2ba025b23
sujal631 <99301344+sujal631@users.noreply.github.com>
2023-02-07 13:52:03 -0600

    database connectivity successful

--- backend/Server.js
+++ backend/Server.js
@@ -1,6 +1,19 @@
 import express from 'express';
 import data from './data.js';
 import images from './images.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('Succeeded to connect to MongoDB');
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
 
 const app = express();
 
