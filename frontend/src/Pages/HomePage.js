40ee0b5677f3289e88de692b24e4dcaa47fc46e7
Vikash Sah <mevkashsah@gmail.com>
2023-02-08 23:33:43 +0545

    pagination added

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,23 +1,27 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Product from '../Components/Product';
-import Slider from '../Components/Slider';
-import { Helmet } from 'react-helmet-async';
-import LoadingComponent from '../Components/LoadingComponent';
-import MessageComponent from '../Components/MessageComponent';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Product from "../Components/Product";
+import Slider from "../Components/Slider";
+import { Helmet } from "react-helmet-async";
+import LoadingComponent from "../Components/LoadingComponent";
+import MessageComponent from "../Components/MessageComponent";
+import Pagination from "../Components/Pagination";
 
 function HomePage() {
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
   const [products, setProducts] = useState([]);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(8);
 
   useEffect(() => {
     const fetchProduct = async () => {
       setLoading(true);
       try {
-        const result = await axios.get('/api/products');
+        const result = await axios.get("/api/products");
         /* const sorted = result.data
           .sort((a, b) => b.views - a.views)
           .slice(0, 5);
@@ -32,6 +36,10 @@ function HomePage() {
     };
     fetchProduct();
   }, []);
+
+  const lastPostIndex = currentPage * postsPerPage;
+  const firstPostIndex = lastPostIndex - postsPerPage;
+  const currentPosts = products.slice(firstPostIndex, lastPostIndex);
 
   return (
     <div>
@@ -50,13 +58,19 @@ function HomePage() {
           <MessageComponent variant="danger">{error}</MessageComponent>
         ) : (
           <Row>
-            {products.map((product) => (
+            {currentPosts.map((product) => (
               <Col key={product.slug} sm={12} md={6} lg={3} className="mb-3">
                 <Product product={product} />
               </Col>
             ))}
           </Row>
         )}
+        <Pagination
+          totalPosts={products.length}
+          postsPerPage={postsPerPage}
+          setCurrentPage={setCurrentPage}
+          currentPage={currentPage}
+        />
       </div>
     </div>
   );
