86ae3a240483824e435b690a4fd903468d1a2ede
sujal631 <99301344+sujal631@users.noreply.github.com>
2023-03-08 10:16:46 -0600

    fix navbar

--- frontend/src/Components/Navigation.js
+++ frontend/src/Components/Navigation.js
@@ -1,5 +1,12 @@
 import React, { useContext } from 'react';
-import { Badge, Container, Navbar, NavDropdown } from 'react-bootstrap';
+import {
+  Badge,
+  Container,
+  Navbar,
+  NavDropdown,
+  Row,
+  Col,
+} from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import Nav from 'react-bootstrap/Nav';
 import { Store } from '../Store';
@@ -24,76 +31,90 @@ export default function Navigation() {
       {/* Dark themed navbar fixed to the top of the page */}
       <Navbar bg="black" variant="dark" expand="lg" fixed="top">
         <Container>
-          {/* Navbar Brand */}
-          <Navbar.Brand className="brand d-flex align-items-center">
-            {/* Logo */}
-            <img
-              src={require('../logo.png')}
-              alt="Logo"
-              style={{
-                width: '90px',
-                height: '75px',
-              }}
-            />
+          <Row className="w-100 align-items-center">
+            {/* Brand Column */}
+            <Col xs={4} className="d-flex align-items-center">
+              <Navbar.Brand className="brand d-flex align-items-center">
+                {/* Logo */}
+                <img
+                  src={require('../logo.png')}
+                  alt="Logo"
+                  style={{
+                    width: '90px',
+                    height: '75px',
+                  }}
+                />
 
-            {/* Brand Name */}
-            <span className="d-none d-lg-block">Best True Value Hardware</span>
-          </Navbar.Brand>
-          <div>
-            <Search />
-          </div>
+                {/* Brand Name */}
+                <span className="d-none d-lg-block">
+                  Best True Value Hardware
+                </span>
+              </Navbar.Brand>
+            </Col>
 
-          {/* Navbar Toggler */}
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            {/* Search Column */}
+            <Col xs={4} className="d-flex justify-content-center">
+              <Search />
+            </Col>
 
-          {/* Navbar Collapse */}
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="right-nav col-md-12">
-              {/* Home Link */}
-              <Link to="/" className="nav-link">
-                Home
-              </Link>
+            {/* Links Column */}
+            <Col xs={4} className="d-flex justify-content-end">
+              {/* Navbar Toggler */}
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
-              {/* Products Link */}
-              <Link to="/products" className="nav-link">
-                Products
-              </Link>
-
-              {/* About Link */}
-              <Link to="/about" className="nav-link">
-                About
-              </Link>
-
-              {userInfo ? (
-                <NavDropdown title={userInfo.firstName} id="basic-nav-dropdown">
-                  <LinkContainer to="/profile">
-                    <NavDropdown.Item>User Profile</NavDropdown.Item>
-                  </LinkContainer>
-                  <LinkContainer to="/orderHistory">
-                    <NavDropdown.Item>Order History</NavDropdown.Item>
-                  </LinkContainer>
-                  <NavDropdown.Divider />
-                  <Link className="dropdown-item" to="/" onClick={logout}>
-                    Log Out
+              {/* Navbar Collapse */}
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="right-nav">
+                  {/* Home Link */}
+                  <Link to="/" className="nav-link">
+                    Home
                   </Link>
-                </NavDropdown>
-              ) : (
-                <LinkContainer to="/login">
-                  <Nav.Link>Log In</Nav.Link>
-                </LinkContainer>
-              )}
 
-              {/* Cart Link */}
-              <Link to="/cart" className="nav-link">
-                <i className="fa fa-shopping-cart"></i>
-                {cart.cartItems.length > 0 && (
-                  <Badge pill bg="danger">
-                    {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
-                  </Badge>
-                )}
-              </Link>
-            </Nav>
-          </Navbar.Collapse>
+                  {/* Products Link */}
+                  <Link to="/products" className="nav-link">
+                    Products
+                  </Link>
+
+                  {/* About Link */}
+                  <Link to="/about" className="nav-link">
+                    About
+                  </Link>
+
+                  {userInfo ? (
+                    <NavDropdown
+                      title={userInfo.firstName}
+                      id="basic-nav-dropdown"
+                    >
+                      <LinkContainer to="/profile">
+                        <NavDropdown.Item>User Profile</NavDropdown.Item>
+                      </LinkContainer>
+                      <LinkContainer to="/orderHistory">
+                        <NavDropdown.Item>Order History</NavDropdown.Item>
+                      </LinkContainer>
+                      <NavDropdown.Divider />
+                      <Link className="dropdown-item" to="/" onClick={logout}>
+                        Log Out
+                      </Link>
+                    </NavDropdown>
+                  ) : (
+                    <LinkContainer to="/login">
+                      <Nav.Link>Log In</Nav.Link>
+                    </LinkContainer>
+                  )}
+
+                  {/* Cart Link */}
+                  <Link to="/cart" className="nav-link">
+                    <i className="fa fa-shopping-cart"></i>
+                    {cart.cartItems.length > 0 && (
+                      <Badge pill bg="danger">
+                        {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                      </Badge>
+                    )}
+                  </Link>
+                </Nav>
+              </Navbar.Collapse>
+            </Col>
+          </Row>
         </Container>
       </Navbar>
     </div>
