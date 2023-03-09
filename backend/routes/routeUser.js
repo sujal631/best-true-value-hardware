a19d4921bce07735923f6ea49f07869512fb66df
sujal631 <99301344+sujal631@users.noreply.github.com>
2023-03-09 11:11:49 -0600

    add comments

--- backend/routes/routeUser.js
+++ backend/routes/routeUser.js
@@ -1,18 +1,26 @@
+// Import required modules and files
 import express from 'express';
 import User from '../models/userModel.js';
 import bcrypt from 'bcryptjs';
 import { isAuth, generateToken } from '../utils.js';
 import expressAsyncHandler from 'express-async-handler';
 
+// Create a new express router
 const routeUser = express.Router();
 
+// Handle user login
 routeUser.post(
   '/login',
   expressAsyncHandler(async (req, res) => {
+    // Get user input from request body
     const { email, password } = req.body;
+    // Find the user with matching email
     const user = await User.findOne({ email });
+    // If the user exists and the provided password matches the stored password
     if (user && bcrypt.compareSync(password, user.password)) {
+      // Extract relevant user data
       const { _id, firstName, lastName, email: userEmail, isAdmin } = user;
+      // Send a response with user data and a generated token
       res.send({
         _id,
         firstName,
@@ -22,21 +30,26 @@ routeUser.post(
         token: generateToken(user),
       });
     } else {
+      // Send an error response
       res.status(401).send({ message: 'Invalid email or password' });
     }
   })
 );
 
+// Handle user registration
 routeUser.post(
   '/registration',
   expressAsyncHandler(async (req, res) => {
+    // Get user input from request body
     const { firstName, lastName, email, password } = req.body;
+    // Create a new user with the provided data
     const user = await User.create({
       firstName,
       lastName,
       email,
       password: bcrypt.hashSync(password),
     });
+    // Extract relevant user data
     const {
       _id,
       firstName: userFirstName,
@@ -44,6 +57,7 @@ routeUser.post(
       email: userEmail,
       isAdmin,
     } = user;
+    // Send a response with user data and a generated token
     res.send({
       _id,
       firstName: userFirstName,
@@ -55,23 +69,29 @@ routeUser.post(
   })
 );
 
+// Handle updating user profile
 routeUser.put(
   '/profile',
   isAuth,
   expressAsyncHandler(async (req, res) => {
+    // Find the user to be updated
     const user = await User.findById(req.user._id);
     if (user) {
+      // Get updated user data from request body
       const {
         firstName = user.firstName,
         lastName = user.lastName,
         email = user.email,
         password,
       } = req.body;
+      // Update user data
       user.firstName = firstName;
       user.lastName = lastName;
       user.email = email;
       if (password) user.password = bcrypt.hashSync(password, 8);
+      // Save updated user data
       const updatedUser = await user.save();
+      // Extract relevant user data
       const {
         _id,
         firstName: userFirstName,
@@ -79,6 +99,7 @@ routeUser.put(
         email: userEmail,
         isAdmin,
       } = updatedUser;
+      // Send a response with updated user data and a generated token
       res.send({
         _id,
         firstName: userFirstName,
@@ -88,9 +109,11 @@ routeUser.put(
         token: generateToken(updatedUser),
       });
     } else {
+      // Send an error response
       res.status(404).send({ message: 'User not found' });
     }
   })
 );
 
+// Export the express router
 export default routeUser;
