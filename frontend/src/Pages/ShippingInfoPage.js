fe4a573796c757cb640b92b95105ac68b838102c
sujal631 <99301344+sujal631@users.noreply.github.com>
2023-03-10 17:55:05 -0600

    add comments- complete

--- frontend/src/Pages/ShippingInfoPage.js
+++ frontend/src/Pages/ShippingInfoPage.js
@@ -1,11 +1,15 @@
+// Import necessary modules
 import React, { useContext, useEffect, useState } from 'react';
-import { Button, Form } from 'react-bootstrap';
+import { Button, Container, Form } from 'react-bootstrap';
 import { Helmet } from 'react-helmet-async';
 import { useNavigate } from 'react-router-dom';
 import CheckoutSteps from '../Components/CheckoutSteps';
+import Message from '../Components/MessageComponent';
 import { Store } from '../Store';
 
+// React component for the Shipping Info page
 export default function ShippingInfoPage() {
+  // A list of options for the state dropdown menu
   const stateOptions = [
     'Select...',
     'Alabama',
@@ -61,29 +65,46 @@ export default function ShippingInfoPage() {
   ];
 
   const navigate = useNavigate();
+  // Get the global state and dispatch function from the Store context
   const { state, dispatch: ctxDispatch } = useContext(Store);
+
+  // Get the user info and shipping info from the global state
   const {
     userInfo,
     cart: { shippingInfo },
   } = state;
-  const [firstName, setFirstName] = useState(shippingInfo.firstName || '');
-  const [lastName, setLastName] = useState(shippingInfo.lastName || '');
-  const [phoneNumber, setPhoneNumber] = useState(
-    shippingInfo.phoneNumber || ''
-  );
-  const [address, setAddress] = useState(shippingInfo.address || '');
-  const [city, setCity] = useState(shippingInfo.city || '');
-  const [region, setRegion] = useState(shippingInfo.region || '');
-  const [zip, setZip] = useState(shippingInfo.zip || '');
-  /*const [country, setCountry] = useState(shippingInfo.country || ''); */
 
+  // Set up state variables using useState hook for each form input
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [address, setAddress] = useState('');
+  const [city, setCity] = useState('');
+  const [region, setRegion] = useState('');
+  const [zip, setZip] = useState('');
+
+  // Use the useEffect hook to set the form inputs to the shipping info stored in the global state
   useEffect(() => {
     if (!userInfo) {
+      // If user is not logged in, redirect to login page with redirect parameter
       navigate('/login?redirect=/shippingInfo');
+    } else {
+      // Set the form input values to the shipping info stored in the global state
+      setFirstName(shippingInfo.firstName || '');
+      setLastName(shippingInfo.lastName || '');
+      setPhoneNumber(shippingInfo.phoneNumber || '');
+      setAddress(shippingInfo.address || '');
+      setCity(shippingInfo.city || '');
+      setRegion(shippingInfo.region || '');
+      setZip(shippingInfo.zip || '');
     }
-  }, [userInfo, navigate]);
-  const submitHandler = (e) => {
+  }, [userInfo, navigate, shippingInfo]);
+
+  // Function to handle form submission
+  const handleSubmit = (e) => {
     e.preventDefault();
+
+    // Dispatch an action to update the shipping info in the global state
     ctxDispatch({
       type: 'SHIPPING_INFO',
       payload: {
@@ -94,9 +115,10 @@ export default function ShippingInfoPage() {
         city,
         region,
         zip,
-        //country,
       },
     });
+
+    // Store the shipping info in local storage
     localStorage.setItem(
       'shippingInfo',
       JSON.stringify({
@@ -107,97 +129,179 @@ export default function ShippingInfoPage() {
         city,
         region,
         zip,
-        //country,
       })
     );
+    // Navigate to the payment method page
     navigate('/paymentMethod');
   };
+
   return (
     <div>
-      {/* Setting the page title using react-helmet-async */}
-      <Helmet>
-        <title>Shipping Info</title>
-      </Helmet>
       <CheckoutSteps step1 step2></CheckoutSteps>
-      <div className="container small-container">
+      <Container className="container small-container">
+        {/* Setting the page title using react-helmet-async */}
+        <Helmet>
+          <title>Shipping Info</title>
+        </Helmet>
+
         {/* Displaying the page header */}
-        <h1 className="my-3">Shipping Info </h1>
-        <Form onSubmit={submitHandler}>
-          <Form.Group className="mb-3" controlId="firstName">
-            <Form.Label>First Name</Form.Label>
-            <Form.Control
-              value={firstName}
+        <h1 className="my-3">Your Information </h1>
+
+        {/* Render the Shipping Info form */}
+        <Form onSubmit={handleSubmit}>
+          {/* A form input for the user's first name */}
+          <div className="mb-3">
+            <label htmlFor="firstName" className="form-label">
+              First Name
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="firstName"
+              required
               onChange={(e) => setFirstName(e.target.value)}
-              required
+              value={firstName}
             />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="lastName">
-            <Form.Label>Last Name</Form.Label>
-            <Form.Control
-              value={lastName}
+          </div>
+
+          {/* A form input for the user's last name */}
+          <div className="mb-3">
+            <label htmlFor="lastName" className="form-label">
+              Last Name
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="lastName"
+              required
               onChange={(e) => setLastName(e.target.value)}
-              required
+              value={lastName}
             />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="phoneNumber">
-            <Form.Label>Phone Number</Form.Label>
-            <Form.Control
-              value={phoneNumber}
-              onChange={(e) => setPhoneNumber(e.target.value)}
+          </div>
+
+          {/* A form input for the user's phone number */}
+          <div className="mb-3">
+            <label htmlFor="phoneNumber" className="form-label">
+              Phone Number
+            </label>
+            <input
+              type="tel"
+              className="form-control"
+              id="phoneNumber"
               required
+              pattern="[0-9]*"
+              onChange={(e) => {
+                // Remove non-digit characters from input
+                const cleaned = e.target.value.replace(/\D/g, '');
+                // Validate cleaned value
+                if (cleaned.length !== 10) {
+                  e.target.setCustomValidity(
+                    'Please enter a valid phone number'
+                  );
+                } else {
+                  e.target.setCustomValidity('');
+                  // Format cleaned value
+                  const formatted = `(${cleaned.slice(0, 3)}) ${cleaned.slice(
+                    3,
+                    6
+                  )}-${cleaned.slice(6, 10)}`;
+                  setPhoneNumber(formatted);
+                }
+              }}
             />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="address">
-            <Form.Label>Address</Form.Label>
-            <Form.Control
-              value={address}
+          </div>
+
+          {/* A form input for the user's physical address */}
+          <div className="mb-3">
+            <Message variant="warning">
+              Please provide a valid cell phone number so that a representative
+              from the store can call you to <strong>PICKUP</strong> your
+              item(s) when they are ready.
+            </Message>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="address" className="form-label">
+              Address
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="address"
+              required
               onChange={(e) => setAddress(e.target.value)}
-              required
+              value={address}
             />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="city">
-            <Form.Label>City</Form.Label>
-            <Form.Control
-              value={city}
+          </div>
+
+          {/* A form input for the user's city */}
+          <div className="mb-3">
+            <label htmlFor="city" className="form-label">
+              City
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="city"
+              required
               onChange={(e) => setCity(e.target.value)}
-              required
+              value={city}
             />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="region">
-            <Form.Label>State</Form.Label>
-            <Form.Select
-              value={region}
-              onChange={(e) => setRegion(e.target.value)}
+          </div>
+
+          {/* A form input for the user's state */}
+          <div className="mb-3">
+            <label htmlFor="region" className="form-label">
+              State
+            </label>
+            <select
+              className="form-control custom-select"
+              id="region"
               required
+              onChange={(e) => setRegion(e.target.value)}
+              value={region}
             >
               {stateOptions.map((state) => (
                 <option key={state} value={state}>
                   {state}
                 </option>
               ))}
-            </Form.Select>
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="zip">
-            <Form.Label>Zip Code</Form.Label>
-            <Form.Control
-              value={zip}
+            </select>
+          </div>
+
+          {/* A form input for the user's zip code */}
+          <div className="mb-3">
+            <label htmlFor="zip" className="form-label">
+              Zip Code
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="zip"
+              required
+              pattern="^\d{5}(?:[-\s]\d{4})?$"
               onChange={(e) => setZip(e.target.value)}
-              required
+              value={zip}
+              onInvalid={(e) => {
+                e.target.setCustomValidity('Please enter a valid zip code');
+              }}
+              onInput={(e) => {
+                e.target.setCustomValidity('');
+              }}
             />
-          </Form.Group>
-          {/*<Form.Group className="mb-3" controlId="country">
-            <Form.Label>Country</Form.Label>
-            <Form.Control
-              value={country}
-              onChange={(e) => setCountry(e.target.value)}
-              required
-            />
-              </Form.Group>*/}
+            <div className="my-3">
+              <Message variant="warning">
+                Please enter your 5-digit zip code (e.g., 12345) or your 9-digit
+                zip code (e.g., 12345-6789).
+              </Message>
+            </div>
+          </div>
+
+          {/* Submit button that triggers handleSubmit function */}
           <div className="d-grid gap-2 mb-3">
             <Button type="submit">CONTINUE</Button>
           </div>
         </Form>
-      </div>
+      </Container>
     </div>
   );
 }
