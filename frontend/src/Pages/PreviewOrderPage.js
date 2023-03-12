be4a7f003bf8e9e663e3947367abd8b5326cf04c
sujal631 <99301344+sujal631@users.noreply.github.com>
2023-03-12 00:33:35 -0600

    add comments - complete

--- frontend/src/Pages/PreviewOrderPage.js
+++ frontend/src/Pages/PreviewOrderPage.js
@@ -1,4 +1,5 @@
-import Axios from 'axios';
+// Import necessary modules
+import axios from 'axios';
 import React, { useContext, useEffect, useReducer } from 'react';
 import { Button, Card, Col, ListGroup, Row } from 'react-bootstrap';
 import { Helmet } from 'react-helmet-async';
@@ -9,6 +10,9 @@ import LoadingSpinner from '../Components/LoadingComponent';
 import { Store } from '../Store';
 import { getErrorMessage } from '../utils';
 
+// Define the initial state of loading for the reducer
+const initialState = { loading: false };
+// Define the reducer function that updates the loading state
 const reducer = (state, action) => {
   const actionHandlers = {
     REQUEST: { ...state, loading: true },
@@ -19,34 +23,41 @@ const reducer = (state, action) => {
   return actionHandlers[action.type] || state;
 };
 
+// Define a helper function to round a number to two decimal places
+const roundToTwoDecimalPlaces = (num) =>
+  Math.round(num * 100 + Number.EPSILON) / 100;
+
 export default function PreviewOrderPage() {
   const navigate = useNavigate();
-  const [{ loading }, dispatch] = useReducer(reducer, {
-    loading: false,
-  });
+  // Get the global state and dispatch function from the Store context
   const { state, dispatch: ctxDispatch } = useContext(Store);
+  // Define a reducer to manage the loading state
+  const [{ loading }, dispatch] = useReducer(reducer, initialState);
+  // Get the cart and user info from the global state
   const { cart, userInfo } = state;
 
-  const roundToTwoDecimalPlaces = (num) =>
-    Math.round(num * 100 + Number.EPSILON) / 100;
-  cart.itemsPrice = roundToTwoDecimalPlaces(
-    cart.cartItems.reduce((a, c) => a + c.quantity * c.price, 0)
-  );
-  cart.taxPrice = roundToTwoDecimalPlaces(0.105 * cart.itemsPrice);
-  cart.totalPrice = cart.itemsPrice + cart.taxPrice;
+  // Check if a payment method has been selected, otherwise redirect to payment method selection page
+  useEffect(() => {
+    if (!cart.paymentMethod) {
+      navigate('/paymentMethod');
+    }
+  }, [cart.paymentMethod, navigate]);
 
-  const placeOrder = async () => {
+  // Define a function to place an order
+  const handlePlaceOrder = async () => {
     try {
+      // Set the loading state to true
       dispatch({ type: 'REQUEST' });
-      const { data } = await Axios.post(
+      // Make a request to the backend to place the order
+      const { data } = await axios.post(
         '/api/orders',
         {
           orderItems: cart.cartItems,
           shippingInfo: cart.shippingInfo,
           paymentMethod: cart.paymentMethod,
-          itemsPrice: cart.itemsPrice,
-          taxPrice: cart.taxPrice,
-          totalPrice: cart.totalPrice,
+          itemsPrice: itemsPrice,
+          taxPrice: taxPrice,
+          totalPrice: totalPrice,
         },
         {
           headers: {
@@ -54,27 +65,36 @@ export default function PreviewOrderPage() {
           },
         }
       );
+
+      // Clear the cart, set the loading state to false, remove cart items from local storage, and navigate to the order confirmation page
       ctxDispatch({ type: 'CLEAR' });
       dispatch({ type: 'SUCCESS' });
       localStorage.removeItem('cartItems');
       navigate(`/order/${data.order._id}`);
+
+      // Set the loading state to false and display an error message
     } catch (error) {
       dispatch({ type: 'FAILURE' });
       toast.error(getErrorMessage(error));
     }
   };
 
-  useEffect(() => {
-    if (!cart.paymentMethod) {
-      navigate('/paymentMethod');
-    }
-  }, [cart, navigate]);
+  // Calculate the item price, tax price, and total price
+  const itemsPrice = roundToTwoDecimalPlaces(
+    cart.cartItems.reduce((a, c) => a + c.quantity * c.price, 0)
+  );
+  const taxPrice = roundToTwoDecimalPlaces(0.105 * itemsPrice);
+  const totalPrice = itemsPrice + taxPrice;
+
   return (
     <div>
+      <CheckoutSteps step1 step2 step3 step4 />
+
       <Helmet>
         <title>Preview Order</title>
       </Helmet>
-      <CheckoutSteps step1 step2 step3 step4 />
+
+      {/* Header of the page*/}
       <h1 className="my-3">Preview Order</h1>
       <Row>
         <Col xs={12} lg={8}>
@@ -84,20 +104,31 @@ export default function PreviewOrderPage() {
                 Your Information
               </Card.Title>
               <Card.Text>
-                <strong>Name: </strong>
-                {cart.shippingInfo.firstName} {cart.shippingInfo.lastName}
-                <br />
-                <strong>Phone Number: </strong>
-                {cart.shippingInfo.phoneNumber}
-                <br />
-                <strong>Address: </strong>
-                {cart.shippingInfo.address}, {cart.shippingInfo.city},{' '}
-                {cart.shippingInfo.region}, {cart.shippingInfo.zip}
+                <div>
+                  <span>
+                    <strong>Name: </strong>
+                    {`${cart.shippingInfo.firstName} ${cart.shippingInfo.lastName}`}
+                  </span>
+                </div>
+                <div>
+                  <span>
+                    <strong>Phone Number: </strong>
+                    {cart.shippingInfo.phoneNumber}
+                  </span>
+                </div>
+                <div>
+                  <span>
+                    <strong>Address: </strong>
+                    {`${cart.shippingInfo.address}, ${cart.shippingInfo.city}, ${cart.shippingInfo.region}, ${cart.shippingInfo.zip}`}
+                  </span>
+                </div>
               </Card.Text>
+
               <Button
                 className="mb-1"
                 variant="secondary"
-                onClick={() => navigate('/shippingInfo')}
+                as={Link}
+                to="/shippingInfo"
               >
                 Edit
               </Button>
@@ -112,7 +143,8 @@ export default function PreviewOrderPage() {
               <Button
                 className="mb-1"
                 variant="secondary"
-                onClick={() => navigate('/paymentMethod')}
+                as={Link}
+                to="/paymentMethod"
               >
                 Edit
               </Button>
@@ -120,39 +152,41 @@ export default function PreviewOrderPage() {
               <Card.Title className="my-1" style={{ color: '#dd2222' }}>
                 Your Items
               </Card.Title>
+
               <ListGroup>
-                {cart.cartItems.map((item) => (
-                  <ListGroup.Item key={item._id}>
-                    <Row className="align-items-center ">
-                      <Col xs={12} lg={12} className="my-2">
-                        <img
-                          src={item.image}
-                          alt={item.name}
-                          className="img-fluid rounded img-thumbnail d-block mb-1"
-                          style={{ maxWidth: '80px', maxHeight: '80px' }}
-                        />
-                        <Link
-                          className="remove-link-style"
-                          to={`/product/${item.slug}`}
-                        >
-                          <strong>{item.name}</strong>
-                        </Link>
-                      </Col>
-                      <Col xs={12} lg={6} className="mb-1  btn-text">
-                        <span>Qty: {item.quantity}</span>
-                      </Col>
-                      <Col xs={12} lg={6} className="mb-1 mx-auto btn-text">
-                        Price: {item.price}
-                      </Col>
-                    </Row>
-                  </ListGroup.Item>
-                ))}
+                {/*  Mapping over an array of items in the cart to render a ListGroup.Item component for each item */}
+                {cart.cartItems.map((item) => {
+                  //  Destructuring the item object to extract its properties
+                  const { _id, image, name, slug, quantity, price } = item;
+                  return (
+                    <ListGroup.Item key={_id}>
+                      <Row className="align-items-center">
+                        <Col xs={12} lg={12} className="my-2">
+                          <img
+                            src={image}
+                            alt={name}
+                            className="img-fluid rounded img-thumbnail d-block mb-1"
+                            style={{ maxWidth: '80px', maxHeight: '80px' }}
+                          />
+                          <Link
+                            className="remove-link-style"
+                            to={`/product/${slug}`}
+                          >
+                            <strong>{name}</strong>
+                          </Link>
+                        </Col>
+                        <Col xs={12} lg={6} className="mb-1 btn-text">
+                          <span>Qty: {quantity}</span>
+                        </Col>
+                        <Col xs={12} lg={6} className="mb-1 mx-auto btn-text">
+                          Price: {price}
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                  );
+                })}
               </ListGroup>
-              <Button
-                variant="secondary"
-                className="mt-3"
-                onClick={() => navigate('/cart')}
-              >
+              <Button variant="secondary" className="mt-3" as={Link} to="/cart">
                 Edit
               </Button>
             </Card.Body>
@@ -166,13 +200,13 @@ export default function PreviewOrderPage() {
                 <ListGroup.Item>
                   <Row>
                     <Col>Item(s)</Col>
-                    <Col>${cart.itemsPrice}</Col>
+                    <Col>${itemsPrice}</Col>
                   </Row>
                 </ListGroup.Item>
                 <ListGroup.Item>
                   <Row>
                     <Col>Tax</Col>
-                    <Col>${cart.taxPrice}</Col>
+                    <Col>${taxPrice}</Col>
                   </Row>
                 </ListGroup.Item>
                 <ListGroup.Item>
@@ -181,19 +215,26 @@ export default function PreviewOrderPage() {
                       <strong>Order Total</strong>
                     </Col>
                     <Col>
-                      <strong>${cart.totalPrice.toFixed(2)}</strong>
+                      <strong>${totalPrice.toFixed(2)}</strong>
                     </Col>
                   </Row>
                 </ListGroup.Item>
                 <ListGroup.Item>
                   <div className="d-grid gap-2 mb-3">
-                    <Button
-                      type="button"
-                      onClick={placeOrder}
-                      disabled={cart.cartItems.length === 0}
-                    >
-                      Place Order
-                    </Button>
+                    {/* If the length of cartItems is 0, the button is disabled */}
+                    {cart.cartItems.length === 0 ? (
+                      <Button type="button" disabled>
+                        Place Order
+                      </Button>
+                    ) : (
+                      {
+                        /* If cartItems has a length greater than 0, the button is enabled and onClick calls the handlePlaceOrder function */
+                      }(
+                        <Button type="button" onClick={handlePlaceOrder}>
+                          Place Order
+                        </Button>
+                      )
+                    )}
                   </div>
                   {loading && <LoadingSpinner />}
                 </ListGroup.Item>
